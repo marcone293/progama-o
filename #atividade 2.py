a324e79308ed6e75c81654e54e12bbe244afb1f3
marcone293 <berserker3780@gmail.com>
2025-04-26 23:24:51 -0300

    Add files via upload

---
+++ #atividade 2.py
@@ -0,0 +1,19 @@
+#atividade 2
+
+alunos =[]
+type(alunos)
+     
+list
+
+alunos=['marcone, gilberto ,thigas']
+alunos.append('marcone')
+     
+
+alunos
+     
+[', Joao,kaua', 'Habacuqui']
+
+alunos_com_notas = [['marcelo', 5.0], ['jonas', 3.0], ['manuela', 1.0]]
+alunos_com_notas
+     
+[['pedrin', 7.0], ['carlos', 4.0], ['marcela', 9.0]]
