fd19e886a1ae10de9ae8690f96b4b3ab767f9101
marcone293 <berserker3780@gmail.com>
2025-04-26 23:27:05 -0300

    Add files via upload

---
+++ ATIVIDADE 1.py
@@ -0,0 +1,71 @@
+# questão 1
+print(5/8)
+
+#realizar uma soma   2
+x= 7
+y= 8
+z= x + y
+print(z)
+
+# questão 3
+a = 5
+b = 3
+
+c = a + b
+
+print("o resultado da soma é", c )
+
+d = 5
+e = 3
+
+f = a - b
+
+print("o resultado da subtracao e é", f )
+
+
+#questão 4
+a = 5* 3
+
+b = 5**3
+
+c=5%3
+
+print(a)
+print(b)
+print(c)
+
+#questão 5
+capital_inicial = 100
+taxa_juros = 0.011
+tempo = 7
+montante =  capital_inicial*(1+taxa_juros)**tempo
+print(montante)
+
+# questrão 6
+poupanca=100
+multiplicador_de_crescimento = 1,1
+desc = "juros compostos"
+ano1 = multiplicador_de_crescimento * poupanca
+
+print(type(ano1))
+
+doubledesc = desc + desc
+print(doubledesc)
+
+#questão 7
+
+pi_string = "3.14159"
+pi_float = float(pi_string)
+print("valor da pi_string", pi_string , "tipo da pi_ string" , type(pi_string))
+
+#questão 8 e 9
+
+salvando = 100
+resultado = 100*1.1**7
+print("Comecei com $"  + str(salvando) +  " e agora tenho $"  + str(resultado) + ". Ótimo!")
+
+#questão 10
+# a letra C esta errada pois o numero 2 não esta convertido para string STR
+
+alunos=['Habacuqui, Joao,kaua']
+alunos.append('Habacuqui')
